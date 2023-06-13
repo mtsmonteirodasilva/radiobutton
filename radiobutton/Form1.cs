d215696d70d16034dfa247de629dfb8989b9743f
Matheus Monteiro <matheus2016monteiro@gmail.com>
2023-06-13 13:43:57 -0400

    radiobutton

---
+++ radiobutton/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radiobutton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sexo = "";
+            if(radioButton1.Checked)
+            {
+                sexo = radioButton1.Text;
+            }
+           else if(radioButton2.Checked)
+            {
+                sexo = radioButton2.Text;
+           }
+
+            MessageBox.Show(sexo);
+    }
+}
