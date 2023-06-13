6add0df4c394b3d20af61aa3eb49aea0b62a45e4
Matheus Monteiro da Silva <101299500+mtsmonteirodasilva@users.noreply.github.com>
2023-06-13 13:50:31 -0400

    Update Form1.cs

--- radiobutton/Form1.cs
+++ radiobutton/Form1.cs
@@ -19,6 +19,8 @@ namespace radiobutton
 
         private void button1_Click(object sender, EventArgs e)
         {
+        //ZECA
+        
             string sexo = "";
             if(radioButton1.Checked)
             {
